03f32faa7edecdc910eb0ae19643702070052dc6
Mukut Brahma <mukutb.mac@gmail.com>
2024-08-08 12:21:00 +0530

    fix: classname

--- src/components/TabButton.tsx
+++ src/components/TabButton.tsx
@@ -1,133 +1,123 @@
-import React, { useMemo, useCallback, useTransition } from "react";
-import SvgIcon, { SvgSupportedIconProps } from "./SvgIcon";
-import CircularProgress from "./CircularProgress";
+import React, { useMemo, useCallback, useTransition } from 'react';
+import SvgIcon, { SvgSupportedIconProps } from './SvgIcon';
+import CircularProgress from './CircularProgress';
+import { twMerge } from 'tailwind-merge';
 
-type SizeVariants = "small" | "large";
+type SizeVariants = 'small' | 'large';
 
-type TabVariants = "primary" | "secondary";
+type TabVariants = 'primary' | 'secondary';
 
 export type Router = {
-  push: (link: string) => unknown | Promise<unknown>;
-  [key: string | number]: unknown;
+    push: (link: string) => unknown | Promise<unknown>;
+    [key: string | number]: unknown;
 }; // Stand in for NextRouter;
 
 export interface TabButtonBaseProps {
-  label: string;
-  isActive?: boolean;
-  size?: SizeVariants;
-  variant?: TabVariants;
-  icon?: SvgSupportedIconProps["icon"];
-  className?: string;
-  onContextMenu?: (
-    e: React.MouseEvent<HTMLAnchorElement | HTMLButtonElement>
-  ) => void;
-  LinkComponent?: React.ElementType;
-  router?: Router;
+    label: string;
+    isActive?: boolean;
+    size?: SizeVariants;
+    variant?: TabVariants;
+    icon?: SvgSupportedIconProps['icon'];
+    className?: string;
+    onContextMenu?: (e: React.MouseEvent<HTMLAnchorElement | HTMLButtonElement>) => void;
+    LinkComponent?: React.ElementType;
+    router?: Router;
 }
 export interface TabLinkButtonProps extends TabButtonBaseProps {
-  link: string;
+    link: string;
 }
 
 export interface TabButtonProps<T = string> extends TabButtonBaseProps {
-  value: T;
-  onClick: (id: T) => void | Promise<void>;
+    value: T;
+    onClick: (id: T) => void | Promise<void>;
 }
 
 const sizeToClassMapping: Record<SizeVariants, string> = {
-  small: "small ",
-  large: "",
+    small: 'small ',
+    large: '',
 };
 
 const variantToClassMapping: Record<TabVariants, string> = {
-  primary: "primary active:pressed",
-  secondary: "secondary active:pressed",
+    primary: 'primary active:pressed',
+    secondary: 'secondary active:pressed',
 };
 
 const variantToActiveClassMapping: Record<TabVariants, string> = {
-  primary: "!border-t-0 !border-r-0 !border-l-0 selected",
-  secondary: "selected",
+    primary: '!border-t-0 !border-r-0 !border-l-0 selected',
+    secondary: 'selected',
 };
 
 export function TabButton<T>({
-  label,
-  isActive,
-  size = "large",
-  variant = "primary",
-  icon,
-  className: cn = "",
-  router,
-  LinkComponent: Link = "a",
-  ...differentiatorProps
+    label,
+    isActive,
+    size = 'large',
+    variant = 'primary',
+    icon,
+    className: cn = '',
+    router,
+    LinkComponent: Link = 'a',
+    ...differentiatorProps
 }: TabButtonProps<T> | TabLinkButtonProps) {
-  const [isPending, startTransitioning] = useTransition();
+    const [isPending, startTransitioning] = useTransition();
 
-  const handleClickButton = useCallback(() => {
-    const { value, onClick } = (differentiatorProps ?? {}) as TabButtonProps<T>;
-    if (typeof value !== "undefined" && onClick) {
-      startTransitioning(() => {
-        new Promise((resolve) => resolve(onClick(value)));
-      });
-    }
-  }, [differentiatorProps]);
-
-  const handleClickLink = useCallback(
-    (e: React.MouseEvent<HTMLAnchorElement>) => {
-      const { link } = (differentiatorProps ?? {}) as TabLinkButtonProps;
-      if (link) {
-        if (e.ctrlKey || e.metaKey) {
-          e.preventDefault();
-          window.open(link, "_blank");
-        } else {
-          startTransitioning(() => {
-            router?.push(link);
-          });
+    const handleClickButton = useCallback(() => {
+        const { value, onClick } = (differentiatorProps ?? {}) as TabButtonProps<T>;
+        if (typeof value !== 'undefined' && onClick) {
+            startTransitioning(() => {
+                new Promise((resolve) => resolve(onClick(value)));
+            });
         }
-      }
-    },
-    [differentiatorProps, router]
-  );
+    }, [differentiatorProps]);
 
-  const className = useMemo(
-    () =>
-      [
-        "tabs inline-flex items-center active:pressed relative",
-        isActive ? variantToActiveClassMapping[variant] : "",
-        variantToClassMapping[variant],
-        sizeToClassMapping[size],
-        cn,
-      ].join(" "),
-    [isActive, cn, size, variant]
-  );
+    const handleClickLink = useCallback(
+        (e: React.MouseEvent<HTMLAnchorElement>) => {
+            const { link } = (differentiatorProps ?? {}) as TabLinkButtonProps;
+            if (link) {
+                if (e.ctrlKey || e.metaKey) {
+                    e.preventDefault();
+                    window.open(link, '_blank');
+                } else {
+                    startTransitioning(() => {
+                        router?.push(link);
+                    });
+                }
+            }
+        },
+        [differentiatorProps, router]
+    );
 
-  const pendingUI = useMemo(
-    () => (
-      <CircularProgress className="ml-core-dimension-25 !size-core-dimension-75" />
-    ),
-    []
-  );
+    const className = useMemo(
+        () =>
+            twMerge(
+                'tabs inline-flex items-center active:pressed relative',
+                isActive ? variantToActiveClassMapping[variant] : '',
+                variantToClassMapping[variant],
+                sizeToClassMapping[size],
+                cn
+            ),
+        [isActive, cn, size, variant]
+    );
 
-  if ((differentiatorProps as TabLinkButtonProps).link) {
-    const tabLinkProps = differentiatorProps as TabLinkButtonProps;
+    const pendingUI = useMemo(() => <CircularProgress className="ml-core-dimension-25 !size-core-dimension-75" />, []);
+
+    if ((differentiatorProps as TabLinkButtonProps).link) {
+        const tabLinkProps = differentiatorProps as TabLinkButtonProps;
+
+        return (
+            <Link href={tabLinkProps.link} className={className} onClick={handleClickLink}>
+                {label}
+                {isPending && pendingUI}
+            </Link>
+        );
+    }
 
     return (
-      <Link
-        href={tabLinkProps.link}
-        className={className}
-        onClick={handleClickLink}
-      >
-        {label}
-        {isPending && pendingUI}
-      </Link>
+        <button onClick={handleClickButton} className={className}>
+            {label}
+            {icon && <SvgIcon icon={icon} />}
+            {isPending && pendingUI}
+        </button>
     );
-  }
-
-  return (
-    <button onClick={handleClickButton} className={className}>
-      {label}
-      {icon && <SvgIcon icon={icon} />}
-      {isPending && pendingUI}
-    </button>
-  );
 }
 
 export default TabButton;
